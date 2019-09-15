5b9722eb5057a73c4c89525a525fa2131e2935fb
Rhonda Kremer <rhondakremer@gmail.com>
2019-09-15 17:11:30 -0400

    added more users

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,7 +18,32 @@ var friendsArray = [
         name: "Casey",
         photo: "https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/36536844_10103886200135677_4391662926981758976_n.jpg?_nc_cat=106&_nc_oc=AQnWx1k0aV3B9P_KmFcT70_IaiSDFC_hzD9OJoHGCYUNS1ttlDnbpugvC17bBugMITI&_nc_ht=scontent-mia3-2.xx&oh=35757500ba37b952749b6b06135f5634&oe=5E06DFA2",
         scores: [
-            
+            3,
+            3,
+            2,
+            3,
+            3,
+            2,
+            4,
+            5,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Nicole",
+        photo: "https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/38920611_10105495547006765_6611115903146786816_n.jpg?_nc_cat=103&_nc_oc=AQl6NqJo21_43uM5t1GP99FgYTAZAfVulTDGbn4x736XTNFrb3o479jYzLqlh8yx1TY&_nc_ht=scontent-mia3-2.xx&oh=f77b552d087b13063dcb7613c4ace1d0&oe=5E39170B",
+        scores: [
+            1,
+            5,
+            1,
+            2,
+            2,
+            1,
+            5,
+            2,
+            4,
+            1
         ]
     }
 ]
